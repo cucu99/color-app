2dfa934d539b00b32889aa1703cbe78b47692d7e
Daniel Kirchner <cucu851223@gmail.com>
2019-05-14 18:27:03 +0200

    clean up starter, added palette

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* TODO: Navbar */}
+        <div className="Palette-colors">{/* TODO: bunch of color boxes */}</div>
+        {/* TODO: Footer */}
+      </div>
+    );
+  }
+}
