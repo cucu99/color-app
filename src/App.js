864730a3f53205b4ff04d2adab1b7a2b391004f4
Daniel Kirchner <cucu851223@gmail.com>
2019-05-19 11:52:21 +0200

    Fix palette route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import seedColors from './colorSeed';
 import { generatePalette } from './ColorHelpers';
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    });
+  }
+
   render() {
     return (
       <Switch>
@@ -12,7 +18,13 @@ class App extends Component {
         <Route
           exact
           path="/palette/:id"
-          render={() => <h1>INDIVIDUAL PALETTE GOES HERE</h1>}
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
         />
       </Switch>
       // <div>
