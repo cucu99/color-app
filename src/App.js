9decf1b535f13bec9802f0bc570d02a60d6ddae5
Daniel Kirchner <cucu851223@gmail.com>
2019-05-20 09:54:45 +0200

    Add Link to ColorBox and add route

--- src/App.js
+++ src/App.js
@@ -33,10 +33,11 @@ class App extends Component {
             />
           )}
         />
+        <Route
+          path="/palette/:paletteId/:colorId"
+          render={() => <h1>Single Color Page!</h1>}
+        />
       </Switch>
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[4])} />
-      // </div>
     );
   }
 }
