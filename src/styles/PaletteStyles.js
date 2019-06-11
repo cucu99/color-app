6393ff00976adff01bd028eb4fdc0705ef623e74
Daniel Kirchner <cucu851223@gmail.com>
2019-06-11 17:09:30 +0200

    Make ColorBox responsive

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
   palette: {
     height: '95vh',
@@ -35,6 +37,19 @@ export default {
       textTransform: 'uppercase',
       border: 'none',
       textDecoration: 'none'
+    },
+    [sizes.down('lg')]: {
+      height: '33.3333%',
+      width: '25%'
+    },
+    [sizes.down('md')]: {
+      height: '20%',
+      width: '50%'
+    },
+
+    [sizes.down('xs')]: {
+      height: '10%',
+      width: '100%'
     }
   }
 };
