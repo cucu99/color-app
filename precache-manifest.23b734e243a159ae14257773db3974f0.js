caee558f77458b6dea3ffd4922c1280b62406d02
Daniel Kirchner <cucu851223@gmail.com>
2019-07-12 19:08:23 +0200

    Updates

---
+++ precache-manifest.23b734e243a159ae14257773db3974f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce02ae4b2bc6de5994e4dd0c1ce4ba7",
+    "url": "/cucu99/ColorApp/index.html"
+  },
+  {
+    "revision": "7ba83e2501db7162dc73",
+    "url": "/cucu99/ColorApp/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "0218990d9d74094aacbe",
+    "url": "/cucu99/ColorApp/static/css/main.05c89b40.chunk.css"
+  },
+  {
+    "revision": "7ba83e2501db7162dc73",
+    "url": "/cucu99/ColorApp/static/js/2.db3520d8.chunk.js"
+  },
+  {
+    "revision": "0218990d9d74094aacbe",
+    "url": "/cucu99/ColorApp/static/js/main.610e7bf1.chunk.js"
+  },
+  {
+    "revision": "f0692367fb107f2104ba",
+    "url": "/cucu99/ColorApp/static/js/runtime~main.23b20141.js"
+  },
+  {
+    "revision": "e106936557f38a1ceaa4eb5889cfbdea",
+    "url": "/cucu99/ColorApp/static/media/bg.e1069365.svg"
+  }
+]);
